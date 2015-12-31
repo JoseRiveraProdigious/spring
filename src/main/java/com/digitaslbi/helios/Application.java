80b053d1bb3c67d1f7af0d8b9b21bbcefe0d8d6c
josriver <jose.rivera@prodigious.com>
2015-12-31 14:51:24 -0500

    Commit

--- src/main/java/com/digitaslbi/helios/Application.java
+++ src/main/java/com/digitaslbi/helios/Application.java
@@ -3,6 +3,7 @@ package com.digitaslbi.helios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
